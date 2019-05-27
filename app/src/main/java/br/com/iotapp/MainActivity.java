545cf5f930709abe6f4689f8e004a483d8d61908
Matheus <matheusdsbarros@gmail.com>
2019-05-27 00:17:17 -0300

    Executando mais de um EndPoint

--- app/src/main/java/br/com/iotapp/MainActivity.java
+++ app/src/main/java/br/com/iotapp/MainActivity.java
@@ -23,55 +23,124 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView valorHumidadeText;
     private TextView valorLuminosidadeText;
     private TextView valorEstadoText;
+    private TextView valorEstadoBuzzerText;
 
-    private Button ligarButton;
-    private Button desligarButton;
+    private Button ligarLedButton;
+    private Button desligarLedButton;
+    private Button ligarBuzzerButton;
+    private Button desligarBuzzerButton;
+    private Button atualizarButton;
 
     JSONObject jObect = null;
     String json = "";
-    String apiUrl = "";
-    String estado = "";
+
+    URL urlApi = null;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ligarButton = findViewById(R.id.ligarButton);
-        desligarButton = findViewById(R.id.desligarButton);
+        ligarLedButton = findViewById(R.id.ligarLedButton);
+        desligarLedButton = findViewById(R.id.desligarLedButton);
+        ligarBuzzerButton = findViewById(R.id.ligarBuzzerButton);
+        desligarBuzzerButton = findViewById(R.id.desligarBuzzerButton);
+
+
+        atualizarButton = findViewById(R.id.atualizarButton);
         valorHumidadeText = findViewById(R.id.valorHumidadeText);
         valorLuminosidadeText = findViewById(R.id.valorLuminosidadeText);
         valorEstadoText = findViewById(R.id.valorEstadoText);
+        valorEstadoBuzzerText = findViewById(R.id.valorEstadoBuzzerText);
         Log.v("MSB", "foi criado");
 
+        String webService = getString(R.string.web_service_url);
 
-        apiUrl = getString(R.string.web_service_url);
-        URL url = createURL(apiUrl);
+        urlApi = createURL(webService);
 
         /*
-            Executa a classe GetApi2 na execução no App.
+            Executa a classe ConnApi na execução no App.
          */
-        getObj(url);
+        ConnectApi(urlApi);
 
     }
 
 
-    /*
-        Executa a classe GetApi2 quando chamada.
-     */
-    public void getObj(URL endereco) {
-        new GetApi2().execute(endereco);
-    }
 
-
+    public void ConnectApi(URL endereco) {new ConnApi().execute(endereco);}
+    public void ConnectApi2(URL endereco) { new ConnApi2().execute(endereco);}
     /*
         Classe que se comunica com a API e Atualiza o MainActivity.
      */
-    class GetApi2 extends AsyncTask<URL, Void, JSONObject> {
+    class ConnApi extends AsyncTask<URL, Void, JSONObject> {
         @Override
         protected JSONObject doInBackground(URL... params) {
             try {
-                Log.v("MSB", "Entrou na classe");
+                HttpURLConnection connection = (HttpURLConnection) params[0].openConnection();
+                InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder sb = new StringBuilder("");
+                String linha = null;
+                while ((linha = reader.readLine()) != null)
+                    sb.append(linha);
+                json = sb.toString();
+                Log.v("MSB", json);
+            } catch (IOException e) {
+
+                e.printStackTrace();
+                return null;
+            }
+            // Transforma a String de resposta em um JSonObject
+            try {
+                jObect = new JSONObject(json);
+            } catch (JSONException e) {
+                Log.e("JSON Parser", "Error parsing data " + e.toString());
+            }
+            return jObect;
+        }
+        /*
+        Recebe o Objeto Json retornado do doInBackground
+         */
+        @Override
+        protected void onPostExecute(JSONObject obj) {
+            try {
+
+                /*
+                    Passa os Valores do Objeto Json para os textView.
+                */
+                valorHumidadeText.setText(obj.getString("humidade"));
+                valorLuminosidadeText.setText(obj.getString("luminosidade"));
+
+
+                String ledd = obj.getString("led");
+                if(ledd.equals("DESLIGADO")){
+                    valorEstadoText.setText("Desligado");
+                }else{
+                    valorEstadoText.setText("Ligado");
+                }
+                String buzz = obj.getString("buzzer");
+                if(buzz.equals("DESLIGADO")){
+                    valorEstadoBuzzerText.setText("Desligado");
+                }else{
+                    valorEstadoBuzzerText.setText("Ligado");
+                }
+                Log.v("MSB","Led: "+ ledd);
+                Log.v("MSB","Buzeer: "+ buzz);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    class ConnApi2 extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... params) {
+            try {
+
                 HttpURLConnection connection = (HttpURLConnection) params[0].openConnection();
                 InputStreamReader isr = new InputStreamReader(connection.getInputStream());
                 BufferedReader reader = new BufferedReader(isr);
@@ -86,30 +155,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 e.printStackTrace();
                 return null;
             }
-            // Transforma a String de resposta em um JSonObject
-            try {
-                jObect = new JSONObject(json);
-                Log.v("MSB", "passou da conversão");
-                Log.v("MSB", jObect.getString("led"));
-            } catch (JSONException e) {
-                Log.e("JSON Parser", "Error parsing data " + e.toString());
-            }
-            return jObect;
+            return json;
         }
-
         /*
         Recebe o Objeto Json retornado do doInBackground
          */
         @Override
-        protected void onPostExecute(JSONObject obj) {
+        protected void onPostExecute(String obj) {
             try {
-                /*
-                    Passa os Valores do Objeto Json para os textView.
-                */
-                valorHumidadeText.setText(obj.getString("humidade"));
-                valorLuminosidadeText.setText(obj.getString("luminosidade"));
-                valorEstadoText.setText(obj.getString("led"));
-            } catch (JSONException e) {
+                ConnectApi(urlApi);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -132,27 +187,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
-            case R.id.ligarButton:
-
+            case R.id.ligarLedButton:
                 //acessa o endPoint que envia o parametro LIGAR para o led
-                apiUrl = getString(R.string.web_service_url_lg);
-                URL url = createURL(apiUrl);
-                if (url != null) {
-                    getObj(url);
+                URL urlLigaLed = createURL(getString(R.string.ws_led_on));
+                if (urlApi != null) {
+                    ConnectApi2(urlLigaLed);
                 } else {
                     Toast.makeText(MainActivity.this, "Falha ao consultar API", Toast.LENGTH_SHORT).show();
                 }
                 break;
 
-            case R.id.desligarButton:
-
+            case R.id.desligarLedButton:
                 //acessa o endPoint que envia o parametro DESLIGAR  para o led
-                apiUrl = getString(R.string.web_service_url_des);
-                URL urlD = createURL(apiUrl);
-                if (urlD != null) {
-                    getObj(urlD);
+                URL urlDesLed = createURL(getString(R.string.ws_led_off));
+                if (urlApi != null) {
+                    ConnectApi2(urlDesLed);
+                } else {
+                    Toast.makeText(MainActivity.this, "Falha ao consultar API", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.ligarBuzzerButton:
+                URL urlLigaBuzzer = createURL(getString(R.string.ws_buzzer_on));
+                if (urlApi != null) {
+                    ConnectApi2(urlLigaBuzzer);
+                } else {
+                    Toast.makeText(MainActivity.this, "Falha ao consultar API", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+
+            case R.id.desligarBuzzerButton:
+                URL urlDesBuzzer = createURL(getString(R.string.ws_buzzer_off));
+                if (urlApi != null) {
+                    ConnectApi2(urlDesBuzzer);
+                } else {
+                    Toast.makeText(MainActivity.this, "Falha ao consultar API", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.atualizarButton:
+                if (urlApi != null) {
+                    ConnectApi(urlApi);
                 } else {
                     Toast.makeText(MainActivity.this, "Falha ao consultar API", Toast.LENGTH_SHORT).show();
                 }
@@ -164,8 +241,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-        ligarButton.setOnClickListener(this);
-        desligarButton.setOnClickListener(this);
+        ligarLedButton.setOnClickListener(this);
+        desligarLedButton.setOnClickListener(this);
+        ligarBuzzerButton.setOnClickListener(this);
+        desligarBuzzerButton.setOnClickListener(this);
+        atualizarButton.setOnClickListener(this);
     }
 
 
